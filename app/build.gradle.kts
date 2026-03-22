055544c4e00c8818d3ce0faa5cde24c9cb8c2964
mimi89999 <michel@lebihan.pl>
2026-03-22 21:13:38 +0100

    Release 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "pl.lebihan.authnkey"
         minSdk = 34
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.2.0"
+        versionCode = 8
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
